e3ca4cbb977ccad0e03be5965fac455106e22d91
Vikt0r1111 <127689762+Vikt0r1111@users.noreply.github.com>
2024-02-16 18:04:49 +0100

    Add files via upload

---
+++ my_project/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sign_in(request):
+    return render(request, "sign_in.html")
+def sign_up(request):
+    return render(request, "sign_up.html")
+def index(request):
+    return render(request, "index.html")
